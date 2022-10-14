a89aabedacab5219df69dd8751c81934030e39ca
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-14 12:44:49 +0200

    Fixing pc number

--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -3,13 +3,14 @@
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 
 - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-- Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+- Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 - Dichiariamo chi ha vinto.
 
 */
 
 //Scelgo il Numero del Pc
-let numeroPerPc = numeroPc;
+let numeroPerPc = numeroPc(1,5);
+console.log("La scelta del Pc è: " + numeroPerPc);
 
 //Genero il Numero per il Pc
 function numeroPc(min, max) {
@@ -21,13 +22,16 @@ function numeroPc(min, max) {
 
 //L'utente sceglie un Numero e Pari o Dispari
 let numeroUtente = Number(prompt("Scegli un numero da 1 a 5: "));
+console.log("La scelta dell'Utente è: " + numeroUtente);
 let pariDispariUtente = prompt("Scegli se pari o dispari: ");
+console.log("La scelta dell'Utente è: " + pariDispariUtente);
 
 //Nel caso in cui l'utente fosse un simpaticone
 while (numeroUtente < 1 || numeroUtente > 5 || isNaN(numeroUtente)) {
 
     alert("Il numero dev'essere da 1 a 5!");
     numeroUtente = parseInt(prompt("Scegli un numero da 1 a 5: "));
+    console.log("La scelta corretta dell'Utente è: " + numeroUtente);
 
 };
 
@@ -35,10 +39,12 @@ while (pariDispariUtente !== "pari" && pariDispariUtente !== "dispari") {
 
     alert("Devi scegliere tra pari o dispari!");
     pariDispariUtente = prompt('Scegli se pari o dispari: ');
+    console.log("La scelta corretta dell'Utente è: " + pariDispariUtente);
 
 };
 
 
+//
 
 
 
